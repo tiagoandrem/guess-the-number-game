d598c790d98a7219e028cabfd23e9e9f431a223b
tiagoandrem <tiagoandrem@hotmail.com>
2019-09-16 14:08:22 +0100

    guess number web Spring Boot vid14

--- web/src/main/java/academy/learnprogramming/util/GameMappings.java
+++ web/src/main/java/academy/learnprogramming/util/GameMappings.java
@@ -5,6 +5,8 @@ public class GameMappings {
     // == constants ==
     public static final String PLAY = "play";
     public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+    public static final String RESTART = "restart";
+    public static final String HOME = "/";
     // == constructor ==
     private GameMappings() {};
 }
